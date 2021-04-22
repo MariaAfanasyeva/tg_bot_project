b2bf04a7426ba6b7928200ee956e4469bcbcf5e9
maryia <masha.afanaseva.29@mail.ru>
2021-04-22 17:40:37 +0300

    added logging

--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -2,12 +2,15 @@ from rest_framework.response import Response
 from ..models import Bot
 from .serializers import BotSerializer
 from rest_framework.decorators import api_view
+import logging
+logging.basicConfig(filename='mainapp.log', format='%(asctime)s - %(levelname)s: %(message)s', filemode='a', level=logging.DEBUG)
 
 
 @api_view(['GET'])
 def bots_list(request):
     bots = Bot.objects.all()
     serializer = BotSerializer(bots, many=True)
+    logging.info('User viewed all bots')
     return Response(serializer.data)
 
 
@@ -15,6 +18,7 @@ def bots_list(request):
 def bot_detail(request, pk):
     bot = Bot.objects.get(id=pk)
     serializer = BotSerializer(bot, many=False)
+    logging.info('User viewed bot with id=%s', pk)
     return Response(serializer.data)
 
 
@@ -23,6 +27,7 @@ def bot_create(request):
     serializer = BotSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+    logging.info('User added a new bot')
     return Response(serializer.data)
 
 
@@ -32,6 +37,7 @@ def bot_update(request, pk):
     serializer = BotSerializer(instance=bot, data=request.data)
     if serializer.is_valid():
         serializer.save()
+    logging.info('User updated bot with id=%s', pk)
     return Response(serializer.data)
 
 
@@ -39,4 +45,5 @@ def bot_update(request, pk):
 def bot_delete(request, pk):
     bot = Bot.objects.get(id=pk)
     bot.delete()
+    logging.info('User deleted bot with id=%s', pk)
     return Response('Deleted')
