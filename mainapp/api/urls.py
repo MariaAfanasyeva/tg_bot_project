ddbab25761ea6653828ea9941634f81db531a163
maryia <masha.afanaseva.29@mail.ru>
2021-04-22 16:15:16 +0300

    added migrations

--- mainapp/api/urls.py
+++ mainapp/api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.bots_list, name="bots"),
-    path('detail/<str:pk>', views.bot_detail, name="detail"),
+    path('detail/<int:pk>', views.bot_detail, name="detail"),
     path('create', views.bot_create, name="create"),
-    path('update/<str:pk>', views.bot_update, name="update"),
-    path('delete/<str:pk>', views.bot_delete, name="delete"),
+    path('update/<int:pk>', views.bot_update, name="update"),
+    path('delete/<int:pk>', views.bot_delete, name="delete"),
 ]
