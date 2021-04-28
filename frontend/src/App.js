e7aa03ae57954b57d9cd94103d22f07246b9d94c
maryia <masha.afanaseva.29@mail.ru>
2021-04-28 16:53:52 +0300

    started handling click event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Bot from "./BotsList";
+import BotsList from "./BotsList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Bot />
+      <BotsList />
     </div>
   );
 }
--- frontend/src/BotsList.js
+++ frontend/src/BotsList.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 
-class Bot extends Component {
+class BotsList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             error: null,
             isLoaded: false,
-            items: []
+            items: [],
+            isSelected: false,
         };
+
+        this.handleClick = this.handleClick.bind(this);
     }
+    handleClick(event) {
+        event.preventDefault();
+        this.setState(state => ({
+            isSelected: !this.state.isSelected
+        }));
+    }
+
 
     componentDidMount() {
         fetch('/api').then(res => res.json()).then(
@@ -28,17 +38,27 @@ class Bot extends Component {
     }
 
     render() {
-        const {error, isLoaded, items} = this.state;
+        const {error, isLoaded, items, isSelected} = this.state;
         if (error) {
             return <p>Error {error.message}</p>
         } else if (!isLoaded) {
             return <p>Loading...</p>
+        } else if (!isSelected) {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            <a onClick={this.handleClick}>{item.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            );
         } else {
             return (
                 <ul>
                     {items.map(item => (
                         <li key={item.name}>
-                            {item.description}
+                            <a onClick={this.handleClick}>{item.name}</a> {item.description}
                         </li>
                     ))}
                 </ul>
@@ -47,4 +67,4 @@ class Bot extends Component {
     }
 }
 
-export default Bot;
+export default BotsList;
