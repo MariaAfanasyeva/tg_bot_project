5ff84fb157889343a396c6ce293c9eb767f2353c
maryia <masha.afanaseva.29@mail.ru>
2021-05-04 18:25:12 +0300

    added custom commands

--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -1,4 +1,4 @@
-from ..models import Bot
+from ..models import Bot, Category
 from .serializers import BotSerializer
 import logging
 from rest_framework import generics
@@ -29,3 +29,14 @@ class UpdateBot(generics.UpdateAPIView):
 class DeleteBot(generics.DestroyAPIView):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
+
+
+
+class GetAllCategoriesList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GetCategoryDetail(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = BotCategoryDetailSerializer
