b5275337880ce2498fe4f504704b4425bdc4cdaa
maryia <masha.afanaseva.29@mail.ru>
2021-05-04 14:30:28 +0300

    added custom commands

---
+++ mainapp/management/commands/create_bot.py
@@ -0,0 +1,16 @@
+from model_bakery import baker
+from mainapp.models import Bot
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Create random bots'
+
+    def add_arguments(self, parser):
+        parser.add_argument('total', type=int, help='Indicates the number of bots to be created')
+
+    def handle(self, *args, **options):
+        total = options['total']
+        for i in range(total):
+            bot = baker.make(Bot)
+            bot.save()
