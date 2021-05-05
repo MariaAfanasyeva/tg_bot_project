b3c8f1f157b1cf4407eb13b1bea0799703fb4a74
maryia <masha.afanaseva.29@mail.ru>
2021-05-05 15:01:12 +0300

    added  django-cors-headers into requirements.txt

--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -16,34 +16,28 @@ class GetAllBotsList(generics.ListAPIView):
 class GetBotDetail(generics.RetrieveAPIView):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
-    pagination_class = CustomPagination
 
 
 class CreateBot(generics.CreateAPIView):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
-    pagination_class = CustomPagination
 
 
 class UpdateBot(generics.UpdateAPIView):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
-    pagination_class = CustomPagination
 
 
 class DeleteBot(generics.DestroyAPIView):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
-    pagination_class = CustomPagination
 
 
 class GetAllCategoriesList(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    pagination_class = CustomPagination
 
 
 class GetCategoryDetail(generics.RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = BotCategoryDetailSerializer
-    pagination_class = CustomPagination
