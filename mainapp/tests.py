52002db3a262573dab2d33f97ba3361d74a189c3
maryia <masha.afanaseva.29@mail.ru>
2021-05-27 11:46:13 +0300

    Changed some tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -23,6 +23,12 @@ jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
 class TestBotsEndpoints(APITestCase):
+    def setUp(self) -> None:
+        self.username = "usuario"
+        self.password = "contrasegna"
+        self.credentials = {"username": self.username, "password": self.password}
+        self.jwt_url = "http://127.0.0.1:8000/api/token/"
+
     def test_get_list(self):
         endpoint = reverse("bots")
         baker.make(Bot, _quantity=4)
@@ -59,6 +65,38 @@ class TestBotsEndpoints(APITestCase):
 
         response = self.client.post(endpoint, expected_json, format="json")
         assert response.status_code == 401
+        assert response.data == expected_json
+
+    def test_create_user_authenticated(self):
+        user = User.objects.create_user(
+            username=self.username,
+            email="usuario@mail.com",
+            password=self.password,
+            id=1,
+        )
+        response = self.client.post(self.jwt_url, self.credentials, format="json")
+        access_key = response.data["access"]
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION="Bearer " + access_key)
+        endpoint = reverse("create")
+        data_json = {
+            "name": "My Bot",
+            "description": "Adorable bot",
+            "link": "https://my_bot",
+            "author": "Nobody",
+        }
+        expected_json = {
+            "name": "My Bot",
+            "description": "Adorable bot",
+            "link": "https://my_bot",
+            "author": "Nobody",
+            "category": None,
+            "auth_user_id": user.id,
+            "id": 1,
+        }
+
+        response = client.post(endpoint, data_json, format="json")
+        assert response.status_code == 201
         assert response.data == expected_json
 
     def test_delete(self):
@@ -103,26 +141,26 @@ class LoginTest(APITestCase):
     def setUp(self) -> None:
         self.username = "usuario"
         self.password = "contrasegna"
-        self.data = {"username": self.username, "password": self.password}
+        self.credentials = {"username": self.username, "password": self.password}
         self.jwt_url = "http://127.0.0.1:8000/api/token/"
 
     def test_incorrect_credentials(self):
-        self.data = {"username": self.username, "password": "wrongPassword"}
-        response = self.client.post(self.jwt_url, self.data)
+        self.credentials = {"username": self.username, "password": "wrongPassword"}
+        response = self.client.post(self.jwt_url, self.credentials)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_correct_credentials(self):
         user = User.objects.create_user(
             username=self.username, email="usuario@mail.com", password=self.password
         )
-        response = self.client.post(self.jwt_url, self.data, format="json")
+        response = self.client.post(self.jwt_url, self.credentials, format="json")
         assert response.status_code == status.HTTP_200_OK
 
     def test_token_is_returned(self):
         user = User.objects.create_user(
             username=self.username, email="usuario@mail.com", password=self.password
         )
-        response = self.client.post(self.jwt_url, self.data, format="json")
+        response = self.client.post(self.jwt_url, self.credentials, format="json")
         assert isinstance(response.data["access"], str)
         assert isinstance(response.data["refresh"], str)
 
@@ -130,41 +168,8 @@ class LoginTest(APITestCase):
         user = User.objects.create_user(
             username=self.username, email="usuario@mail.com", password=self.password
         )
-        response = self.client.post(self.jwt_url, self.data, format="json")
+        response = self.client.post(self.jwt_url, self.credentials, format="json")
         access_key = response.data["access"]
         secret_key = dotenv_values(".env.dev")["SECRET_KEY"]
         decoded = jwt.decode(access_key, secret_key, algorithms="HS256")
         assert decoded["token_type"] == "access"
-
-    def test_create_user_authenticated(self):
-        user = User.objects.create_user(
-            username=self.username,
-            email="usuario@mail.com",
-            password=self.password,
-            id=1,
-        )
-        response = self.client.post(self.jwt_url, self.data, format="json")
-        access_key = response.data["access"]
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION="Bearer " + access_key)
-        endpoint = reverse("create")
-        data_json = {
-            "name": "My Bot",
-            "description": "Adorable bot",
-            "link": "https://my_bot",
-            "author": "Nobody",
-            "id": 10,
-        }
-        expected_json = {
-            "name": "My Bot",
-            "description": "Adorable bot",
-            "link": "https://my_bot",
-            "author": "Nobody",
-            "category": None,
-            "auth_user_id": user.id,
-            "id": 10,
-        }
-
-        response = client.post(endpoint, data_json, format="json")
-        assert response.status_code == 201
-        assert response.data == expected_json
