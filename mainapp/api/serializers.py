337f1cb52e8410d3a09c8b5b0a7b08d5b13844bb
maryia <masha.afanaseva.29@mail.ru>
2021-06-14 17:17:21 +0300

    added feature flag to link validation

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -1,4 +1,7 @@
+import ldclient
+import requests
 from django.contrib.auth.models import User
+from ldclient.config import Config
 from rest_framework import serializers
 
 from ..models import Bot, Category, Comment
@@ -25,6 +28,24 @@ class BotSerializer(serializers.ModelSerializer):
         allow_empty=True,
         required=False,
     )
+    ldclient.set_config(Config("sdk-c213fb71-0a88-44e3-907d-bb83ed45b8a2"))
+
+    ld_client = ldclient.get()
+    user = {
+        "key": "1111",
+    }
+
+    show_feature = ldclient.get().variation("link-validation", user, False)
+
+    if show_feature:
+
+        def validate_link(self, value):
+            response = requests.get(value)
+            if response.status_code != 200:
+                raise serializers.ValidationError("Invalid link")
+            return value
+
+    ldclient.get().close()
 
     class Meta:
         model = Bot
