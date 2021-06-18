74e98392f1938ce8f65676594aee97b1a843ff2b
maryia <masha.afanaseva.29@mail.ru>
2021-06-18 16:59:44 +0300

    Added audit

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Bot, Category, Comment, Like
+from .models import Audit, Bot, Category, Comment, Like
 
 admin.site.register(Bot)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(Audit)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,9 @@
+import inspect
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
 
 
 class Category(models.Model):
@@ -75,3 +79,96 @@ class Like(models.Model):
 
     def __str__(self):
         return f"like to {self.to_bot} by {self.author}"
+
+
+class Audit(models.Model):
+    create_time = models.DateTimeField(
+        verbose_name="create time", auto_now_add=True, null=True, blank=True
+    )
+    table_name = models.CharField(
+        max_length=50, verbose_name="table", null=True, blank=True
+    )
+    field_name = models.CharField(
+        max_length=75, verbose_name="field", null=True, blank=True
+    )
+    old_value = models.TextField(verbose_name="old value", null=True, blank=True)
+    new_value = models.TextField(verbose_name="new value", null=True, blank=True)
+    author = models.CharField(
+        max_length=40, verbose_name="author", null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.author} changed table {self.table_name} at {self.create_time}"
+
+
+@receiver(pre_save, sender=Category)
+@receiver(pre_save, sender=Bot)
+@receiver(pre_save, sender=Comment)
+@receiver(pre_save, sender=Like)
+def pre_save_signal_handler(sender, instance, **kwargs):
+    for frame_record in inspect.stack():
+        if frame_record[3] == "get_response":
+            request = frame_record[0].f_locals["request"]
+            user = request.user
+            break
+    else:
+        request = None
+        user = None
+    model_fields = []
+    for i in instance._meta.fields:
+        model_fields.append(i.name)
+    new_obj = instance
+    model_name = sender.__name__
+    if instance.id:
+        old_obj = sender.objects.get(id=instance.id)
+        for field in model_fields:
+            if getattr(old_obj, field) != getattr(new_obj, field):
+                old_val = sender._meta.get_field(field).value_from_object(old_obj)
+                new_val = sender._meta.get_field(field).value_from_object(new_obj)
+                Audit.objects.create(
+                    table_name=model_name,
+                    field_name=field,
+                    old_value=old_val,
+                    new_value=new_val,
+                    author=user,
+                )
+    else:
+        for field in model_fields:
+            new_val = sender._meta.get_field(field).value_from_object(new_obj)
+            Audit.objects.create(
+                table_name=model_name,
+                field_name=field,
+                old_value=None,
+                new_value=new_val,
+                author=user,
+            )
+
+
+@receiver(pre_delete, sender=Category)
+@receiver(pre_delete, sender=Bot)
+@receiver(pre_delete, sender=Comment)
+@receiver(pre_delete, sender=Like)
+def pre_delete_signal_handler(sender, instance, **kwargs):
+    for frame_record in inspect.stack():
+        if frame_record[3] == "get_response":
+            request = frame_record[0].f_locals["request"]
+            user = request.user
+            break
+    else:
+        request = None
+        user = None
+    model_fields = []
+    for i in instance._meta.fields:
+        model_fields.append(i.name)
+        print(i.name)
+    model_name = sender.__name__
+    old_obj = sender.objects.get(id=instance.id)
+    for field in model_fields:
+        old_val = sender._meta.get_field(field).value_from_object(old_obj)
+        Audit.objects.create(
+            table_name=model_name,
+            field_name=field,
+            old_value=old_val,
+            new_value=None,
+            author=user,
+        )
